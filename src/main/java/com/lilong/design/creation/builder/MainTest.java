d378f9ddcd20313bb5aacc75b2859db09292cc21
lilong <1970404793@qq.com>
2024-09-27 23:03:40 +0800

    doc:注释

--- src/main/java/com/lilong/design/creation/builder/MainTest.java
+++ src/main/java/com/lilong/design/creation/builder/MainTest.java
@@ -3,7 +3,7 @@ package com.lilong.design.creation.builder;
 /**
  * @author : lilong
  * @date : 2024-09-27 22:58
- * @description :
+ * @description : 建造者模式与工厂模式的区别:建造者模式是屏蔽过程不屏蔽细节
  */
 public class MainTest {
     public static void main(String[] args) {
