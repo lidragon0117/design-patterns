6ce01f78ecfef168d8a24c7643890c7d10e8c409
lilong <1970404793@qq.com>
2024-10-06 11:23:17 +0800

    feat : 外观模式

--- src/main/java/com/lilong/design/structural/facade/MainTest.java
+++ src/main/java/com/lilong/design/structural/facade/MainTest.java
@@ -3,7 +3,7 @@ package com.lilong.design.structural.facade;
 /**
  * @author : lilong
  * @date : 2024-10-06 11:13
- * @description :
+ * @description : 外观模式
  */
 public class MainTest {
     public static void main(String[] args) {
