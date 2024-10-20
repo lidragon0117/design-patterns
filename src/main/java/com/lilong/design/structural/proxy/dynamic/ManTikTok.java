c1a07b325fbcf64347755da9b565f8d649ef6270
lilong <1970404793@qq.com>
2024-10-20 21:30:59 +0800

    feat: 二十三种模式全

---
+++ src/main/java/com/lilong/design/structural/proxy/dynamic/ManTikTok.java
@@ -0,0 +1,5 @@
+package com.lilong.design.structural.proxy.dynamic;
+
+public  interface ManTikTok {
+   void tiktok();
+}
---
+++ src/main/java/com/lilong/design/structural/proxy/dynamic/SellTikTok.java
@@ -0,0 +1,6 @@
+package com.lilong.design.structural.proxy.dynamic;
+
+public interface SellTikTok {
+
+    void sell();
+}
